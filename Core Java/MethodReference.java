51b4fdfdf92f7ea2c6955962674801240c5d7e12
shailesh2909 <shaileshsuryawanshi443103@gmail.com>
2026-04-16 19:51:39 +0530

    Method Reference

--- Core Java/MethodReference.java
+++ Core Java/MethodReference.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class MethodReference {
-    
+
     public static void main(String[] args) {
        
         List<String> list = Arrays.asList("Java", "Python", "C++", "Go");
@@ -9,7 +9,6 @@ public class MethodReference {
         List<String> uName = list.stream()
                 .map(String::toUpperCase)
                 .toList();
-        
 
         uName.forEach(System.out::println);
     }
