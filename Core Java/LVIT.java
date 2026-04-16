d13c8757d8a197a5a04d3d1320f90d058733df33
shailesh2909 <shaileshsuryawanshi443103@gmail.com>
2026-04-16 19:54:23 +0530

    LVTI

---
+++ Core Java/LVIT.java
@@ -0,0 +1,15 @@
+public class LVIT {
+    
+    public static void main(String[] args) {
+        
+        var a = 10;
+        var b = 20;
+
+        var sum = a + b;
+
+        var arr = new int[10];
+
+        System.out.println("Sum: " + sum);
+        
+    }
+}
